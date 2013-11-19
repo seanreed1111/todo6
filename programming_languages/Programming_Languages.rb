d6d24abc3a1a913cfa93fb88e2fd83eace9ab114
Sean Reed <seanreed1111@gmail.com>
2013-11-19 15:22:52 -0500

    adds working Programming_Languages.rb file

--- programming_languages/Programming_Languages.rb
+++ programming_languages/Programming_Languages.rb
@@ -1,6 +1,6 @@
-Organizing Nested Data
+# Organizing Nested Data
 
-I have a collection of Programming Languages.  
+# I have a collection of Programming Languages.  
 languages = {
   :oo => {
     :ruby => {
@@ -33,16 +33,38 @@ languages = {
   }
 }
 
-I would like you to reformat the nested structure to be of the following form
+# I would like you to reformat the nested structure to be of the following form
 
-{
-  :ruby => {
-    :style => [:oo],
-    :type => "dynamic"
-  },
-  :python => {
-    :style => [:oo],
-    :type => "dynamic"
-  }
-  etc etc
-}
+# language = {
+#   :ruby => {
+#     :style => [:oo],
+#     :type => "interpreted"
+#   },
+#   :python => {
+#     :style => [:oo],
+#     :type => "interpreted"
+#   }
+#   etc etc
+# }
+
+new_lang = {}
+languages.each do |key, value| # key = :oo, :functional ; value = {hash with key=language names}
+  value.each_key do |key2|
+
+    if new_lang[key2].nil?
+      new_lang[key2] = {}
+      new_lang[key2][:style] = key
+    end
+
+    value.each_value do |value2|
+      value2.each do |key3, value3|
+        new_lang[key2][key3] = value3
+      end
+    end
+
+    
+  end
+
+
+end
+puts "new_lang is #{new_lang}"
